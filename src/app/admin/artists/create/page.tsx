444a2c9428b834e61a0ae7cf356bbe18241e9246
BandFlow25 <jason@flowency.co.uk>
2025-11-04 22:06:58 +0000

    Fully remove admin pages and components

--- src/app/admin/artists/create/page.tsx
+++
@@ -1,9 +0,0 @@
-// /app/admin/artists/create/page.tsx
-"use client";
-
-import { ArtistEdit } from "@/components/admin/edit/ArtistEdit";
-
-export default function CreateArtistPage() {
-  // No artistId is passed, which puts the component in "create" mode
-  return <ArtistEdit />;
-}
