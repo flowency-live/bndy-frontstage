bc70f40f77e716d051f7a96204532ca3b015a210
BandFlow25 <jason@flowency.co.uk>
2025-11-05 17:46:25 +0000

    Revert to CARTO tiles with balanced blue filter

--- src/components/map/LeafletSettings/TileProviders.ts
+++ src/components/map/LeafletSettings/TileProviders.ts
@@ -1,9 +1,9 @@
 // Configure the tile layer - Google Maps style blue appearance
-// Uses Stadia Maps Alidade Smooth for clean blue aesthetic
+// Uses CARTO Positron with CSS filter for blue aesthetic
 // Theme toggle does NOT affect map tiles (only UI overlays)
 
 export const tileLayer = {
-  url: "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png",
+  url: "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
   className: "map-tiles-blue",
-  // attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  // attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
 };
