5235159d37722add658e6034c95e568db9300dec
BandFlow25 <jason@flowency.co.uk>
2025-11-05 10:04:44 +0000

    test: Swap light/dark tile URLs to verify deployment and code execution

--- src/components/map/LeafletSettings/TileProviders.ts
+++ src/components/map/LeafletSettings/TileProviders.ts
@@ -3,13 +3,13 @@
 // Mimics the Google Maps blue hue style from previous implementation (#242a38)
 
 export const lightTileLayer = {
-  url: "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+  url: "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", // SWAPPED FOR TESTING
   className: "map-tiles-light" // CSS class for blue hue filter
   // attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
 };
 
 export const darkTileLayer = {
-  url: "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+  url: "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", // SWAPPED FOR TESTING
   className: "map-tiles-dark" // CSS class for softer dark mode
   // attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
 };
