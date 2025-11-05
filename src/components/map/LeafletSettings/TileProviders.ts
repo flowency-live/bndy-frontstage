9bd8a42c717994bd8fadc61a2c2b7ab9f480ed79
BandFlow25 <jason@flowency.co.uk>
2025-11-05 15:05:56 +0000

    Switch to Stadia Maps tile provider for native blue appearance

--- src/components/map/LeafletSettings/TileProviders.ts
+++ src/components/map/LeafletSettings/TileProviders.ts
@@ -1,9 +1,9 @@
-// Configure the tile layer - single consistent blue-gray style
-// Uses CARTO Positron with CSS filter for subtle blue aesthetic
+// Configure the tile layer - Google Maps style blue appearance
+// Uses Stadia Maps Alidade Smooth for clean blue aesthetic
 // Theme toggle does NOT affect map tiles (only UI overlays)
 
 export const tileLayer = {
-  url: "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
-  className: "map-tiles-blue", // CSS class for blue-gray filter
-  // attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+  url: "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png",
+  className: "map-tiles-blue",
+  // attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 };
