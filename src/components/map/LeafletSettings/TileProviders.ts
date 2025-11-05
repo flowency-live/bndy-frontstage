db1c77888c4496589519a855b057f3a3d757d24f
BandFlow25 <jason@flowency.co.uk>
2025-11-05 17:58:21 +0000

    Switch to CARTO Voyager tiles with built-in blue water colors

--- src/components/map/LeafletSettings/TileProviders.ts
+++ src/components/map/LeafletSettings/TileProviders.ts
@@ -1,9 +1,9 @@
 // Configure the tile layer - Google Maps style blue appearance
-// Uses CARTO Positron with CSS filter for blue aesthetic
+// Uses CARTO Voyager - has natural blue water colors built-in
 // Theme toggle does NOT affect map tiles (only UI overlays)
 
 export const tileLayer = {
-  url: "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+  url: "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
   className: "map-tiles-blue",
   // attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
 };
